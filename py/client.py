4633452dfbece55ea6ed7fd2e31b6204ff19b52a
Liu Hao <harsonlau@gmail.com>
2022-08-12 13:59:07 +0800

    added logging

--- py/client.py
+++ py/client.py
@@ -1,12 +1,14 @@
 import python_gen as gpuprofiling
 import argparse
 import grpc
+import logging
 def getProfilingResponseFromPB(s:str)->gpuprofiling.GPUProfilingResponse:
     with open(s, "rb") as f:
         return gpuprofiling.GPUProfilingResponse.FromString(f.read())
 def getProfilingResponseFromFile(s:str)->gpuprofiling.GPUProfilingResponse:
     return gpuprofiling.GPUProfilingResponse()
 if __name__ =="__main__":
+    logging.basicConfig(level=logging.INFO)
     parser=argparse.ArgumentParser()
     parser.add_argument('--addr',type=str,dest='addr',default='localhost:8886',help='the address to connect to')
     parser.add_argument('--duration',type=int,dest='duration',default=2000,help='PC sampling duration')
@@ -16,10 +18,13 @@ if __name__ =="__main__":
     args=parser.parse_args()
     r=gpuprofiling.GPUProfilingResponse()
     if args.pbfn is not None:
+        logging.info(f'loading response from pre-dumped pb: {args.pbfn}')
         r=getProfilingResponseFromPB(args.pbfn)
     elif args.dumpfn is not None:
+        logging.info(f'loading response from pre-dumped txt: {args.dumpfn}')
         r=getProfilingResponseFromFile(args.dumpfn)
     else:
+        logging.info('no dumped file, issuing rpc request')
         with grpc.insecure_channel(args.addr) as channel:
             stub = gpuprofiling.GPUProfilingServiceStub(channel)
             r=stub.PerformGPUProfiling(gpuprofiling.GPUProfilingRequest(duration=args.duration))
